fbe4273e733dd8fa7623eab7efe9884b546dbbd4
Qiming Chu <cchuqiming@gmail.com>
2024-06-07 22:44:53 +0800

    Add nix flake

---
+++ default.nix
@@ -0,0 +1,47 @@
+{
+  pkgs ? import <nixpkgs> {},
+  system ? builtins.currentSystem,
+}: let
+  gtkwaveNightly = pkgs.stdenv.mkDerivation {
+    pname = "gtkwave";
+    version = "latest";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "gtkwave";
+      repo = "gtkwave";
+      rev = "27f3a94aede651f232314d0fe0c86cc35308fa85";
+      sha256 = "sha256-RxEI1dTMrrFccxuYkb9GQzuFrzTiWOnnfEBR7V918c8=";
+    };
+
+    buildInputs = with pkgs; [
+      flex
+      meson
+      ninja
+      pkg-config
+      gtk3
+      glib
+      gperf
+      gobject-introspection
+      desktop-file-utils
+      shared-mime-info
+    ];
+
+    nativeBuildInputs = with pkgs; [
+      meson
+      ninja
+    ];
+
+    configurePhase = ''
+      meson setup build --prefix=$out
+    '';
+
+    buildPhase = ''
+      meson compile -C build
+    '';
+
+    installPhase = ''
+      meson install -C build
+    '';
+  };
+in
+  gtkwaveNightly
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Install GTKWave with the latest version";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    outputs = flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      gtkwaveOverlay = import ./default.nix {
+        inherit system pkgs;
+      };
+
+      apps = rec {
+        default = apps.gtkwave;
+        gtkwave = flake-utils.lib.mkApp {drv = gtkwaveOverlay;};
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gtkwaveOverlay
+        ];
+      };
+
+      # nix fmt
+      formatter = pkgs.alejandra;
+    });
+  in
+    outputs
+    // {
+      # Overlay that can be imported so you can access the packages
+      # Use `gtkwaveNightly` to access the package
+      overlays.default = final: prev: {
+        gtkwaveNightly = outputs.overlays.gtkwaveNightly;
+      };
+    };
+}
